843a5c6c0a8b1e824abe994182ed1446ea130ce3
srdplas <cabrerozayas199@gmail.com>
2021-12-13 08:28:29 +0100

    Boletin 2 acabado

--- Boletin 2.sql
+++ Boletin 2.sql
@@ -44,14 +44,14 @@ CREATE TABLE departamentos(
     presupuesto INTEGER,
     pres_actual INTEGER,
     constraint departamentos_pk primary key (dpto_cod),
-    constraint jefe_fk foreign key (jefe) REFERENCES empleados(dni)
+    constraint jefe_fk foreign key (jefe) REFERENCES empleados(dni) on update cascade on delete cascade
 );
 -- Tabla estudios con fk en tabla universidades y empleados
 CREATE TABLE estudios(
     empleado_dni int(8),
     universidad int(5),
     año SMALLINT,
-    grado varchar(3),
+    grado varchar(30),
     especialidad varchar(20),
     constraint estudios_empleado_fk foreign key (empleado_dni) references empleados (dni) ON UPDATE cascade ON DELETE CASCADE,
     constraint estudios_universidad_fk foreign key (universidad) references universidades (univ_cod) ON UPDATE cascade ON DELETE CASCADE
@@ -80,8 +80,8 @@ CREATE TABLE historial_salarial(
     constraint historial_salarial_fk foreign key (empleado_dni) references empleados(dni) ON UPDATE CASCADE ON DELETE CASCADE
     
 );
-alter table historial_salarial add constraint salario_inicio_fk foreign key (fecha_comienzo) references historial_laboral(fecha_inicio);
-alter table historial_salarial add constraint salario_fin_fk foreign key (fecha_fin) references historial_laboral(fecha_fin);
+--alter table historial_salarial add constraint salario_inicio_fk foreign key (fecha_comienzo) references historial_laboral(fecha_inicio);
+--alter table historial_salarial add constraint salario_fin_fk foreign key (fecha_fin) references historial_laboral(fecha_fin);
 
 /* ORDEN AL INSERTAR LAS TABLAS
 1 empleados          5 estudios 
@@ -124,11 +124,11 @@ insert into trabajos values(3, 'Barrendero', 540.45, 785.82);
 insert into universidades values(4, 'Palo Olavide', 'Sevilla', 'Antequera', '78095');
 insert into universidades values(6, 'Universidad Sevilla', 'Sevilla', 'Bermejales', '42095');
 
-insert into departamentos values(11, 'Recursos Humanos', 13354, 3000, 2500);
+insert into departamentos values(11, 'Recursos Humanos', '85678432', 3000, 2500);
 insert into departamentos values(9, 'Mano de obra', null, 2000, 1500);
 
-insert into estudios values('85678432', 6, '2018', 'INF', 'Programacion Objetos');
-insert into estudios values('85328432', 4, '2017', 'RPP', 'Marketing WEB');
+insert into estudios values('85678432', 6, '2018', 'INFORMATICA', 'Programacion Objetos');
+insert into estudios values('85328432', 4, '2017', 'RELACIONES PUBLICAS', 'Marketing WEB');
 
   
 insert into historial_laboral values('85678432', 1, '2021-12-12', null, 11, null);
@@ -141,17 +141,19 @@ insert into historial_salarial values('85328432', 600, '2018-12-12', '2020-12-13
 -- ---------------------------------------------------------------------------------------------------------------- --
 
 alter table empleados disable keys;
-alter table empleados enable keys;
 
 insert into empleados values('111222', 'Sergio', 'Palma', 'Entrena', null, null, null, null, null, 'P', null);
 insert into empleados values('222333', 'Lucia', 'Ortega', 'Plus', null, null, null, null, null, null, null);
--- --
 
 insert into historial_laboral values ('111222', null, '1996-06-16', null, null, '222333');
+alter table empleados enable keys;
 
 /*
 Borramos la constraint nos deja introducir los valores y para añadirla de nuevo
 tenemos que borrar los datos introducidos para poder volver a poner la restriccion
+alter table empleados drop constraint sexo_ck;
+alter table empleados add constraint sexo_ck CHECK (sexo='H' OR sexo='M');
+
 */
 
 -- Si asignamos un supervisor que no está en la lista de empleados, violaría la segunda regla de integridad, puede ser nulo pero 
@@ -172,12 +174,48 @@ delete from universidades where univ_cod=4;
 -- Añadimos restriccion para que las personas de sexo masculino tengan que tener el campo de f_nacimiento en not null
 alter table empleados add constraint hombre_check check (fecha_nac is not null where sexo='H');
 
+-- Añadimos un nuevo atributo a la tabla de empleados , valoracion con un valor por defecto de 5
+alter table empleados add column valoracion DECIMAL (4,2) default 5;
+-- Nada mas insertarlo como no le hemos dado valores , la valoracion de las tuplas de todos los empleados se pone a 5
+insert into empleados (dni, nombre, apellido1, apellido2, direcc1, direcc2, ciudad, municipio, cod_postal, sexo, fecha_nac) 
+values('25678943','Carmen', 'Perez', 'Gomez', 'c/ tomas destro n1º 13', null, 'Malaga', 'Marbella', '61230', 'M', '1995-01-07');
+-- Insertamos todos los campos menos el de valoracion y por defecto en vez de null pone 5 como hemos puesto antes.
 
+-- Borramos la restriccion de que el nombre del empleado no pueda ser nulo
 
+alter table empleados modify nombre VARCHAR(10);
 
+-- Modificamos la columna de direc1 para que sea una cadena de caracteres como maximo de 40
+alter table empleados modify direcc1 varchar(40),
 
+-- Modificamos el tipo de dato del codigo de empleado, dni
+alter table empleados modify dni varchar(40); -- no nos dejaría porque es foreign key en otras tablas y todas deben ser del mismo tipo
+-- Habría que cambiar el tipo de dato de todas las columnas de las tablas a las que hace referencia y ponerlo al mismo tipo
 
+-- Modificamos el tipo de dato de la fecha de nacimiento de los empleados
+alter table empleados modify fecha_nac varchar(40); 
+-- Nos deja cambiarle el tipo de dato, ya que no hace referencia en otra tabla como en el caso anterior
 
+-- cambiamos la primary key de empleado a nombre y apellidos
+alter table empleados drop constraint empleado_pk;
+alter table empleados add constraint empleado_pk primary key (nombre, apellido1, apellido2);
+-- Error on rename of '.\boletin2\#sql-alter-2780-1e' to '.\boletin2\empleados' (errno: 150 "Foreign key constraint is incorrectly formed")
+-- Me sale ese error al intentar borrar la primary key pero esto funciona porque se hizo en el boletin 1.
 
+delete from empleados where nombre='Yuri'; -- borramos al empleado yuri
+-- como le tenemos puesto on update cascade on delete cascade, el empleado Yuri ha sido borrado de historial_laboral
+-- y tambien de la tabla historial_salarial.
 
+-- Creamos una tabla con la concatenacion de nombre, apellido1, apellido2 y universidad de los datos de dos tablas
+create table informacion_universitaria as select concat(nombre, ' ', apellido1, ' ', apellido2, ' ', nombre_univ) as info_empleado from empleados, universidades;
 
+-- borramos las tablas en el orden inverso en el las creamos ya que al tener foreign keys no nos dejarán borrarla
+drop table historial_salarial;
+drop TABLE historial_laboral;
+drop TABLE estudios;
+drop table departamentos;
+drop TABLE universidades;
+drop TABLE trabajos;
+drop TABLE empleados;
+-- informacion universitaria no tiene fk el orden en esta es indiferente
+drop TABLE informacion_universitaria;
